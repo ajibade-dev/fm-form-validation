1f822cfe43146c7433c40c85bf514909c975286b
unknown <ajibadeifedolapo17@gmail.com>
2023-07-01 11:14:47 +0100

    Pushing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ const navigate = useNavigate()
 
   return (
     <div className='flex justify-center items-center bg-dark-slate-grey font-roboto md:p-32 p-0'>
-        <div className='flex md:flex-row flex-col-reverse bg-white rounded-2xl h-full shadow-md items-center'>
+        <div className='flex md:flex-row flex-col-reverse bg-white md:rounded-2xl rounded-none h-full shadow-md items-center'>
             <div className='flex flex-col w-full gap-5 p-16'>
                 <h1 className='md:text-6xl text-3xl font-bold font-roboto text-dark-slate-grey'>Stay Updated!</h1>
                 <p className='font-roboto text-dark-slate-grey text-md'>Join 60,000 plus product managers receiving monthly updates on:</p>
